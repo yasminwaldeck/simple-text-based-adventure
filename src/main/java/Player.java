4ad8bdd862672f2f6eabcfd8a7b3c54853f3082e
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-04-25 00:39:29 +0200

    Add set methods to Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,9 @@
 public class Player {
 
+
+    // Set Up
+
+
     private int xp;
     private int level;
     private int hp;
@@ -14,25 +18,52 @@ public class Player {
         luck = 0.5;
     }
 
+    //Set & Get
+
     public int getXP(){
         return xp;
+    }
+
+    public void setXP(int n){
+        xp = n;
     }
 
     public int getLevel(){
         return level;
     }
 
+    public void setLevel(int n){
+        level = n;
+    }
+
+
     public int getHP(){
         return hp;
     }
+
+    public void setHP(int n){
+        hp = n;
+    }
+
 
     public int getMaxHP(){
         return maxHP;
     }
 
+    public void setMaxXP(int n){
+        maxHP = n;
+    }
+
+
     public int getLuck(){
         return luck;
     }
+
+    public void setLuck(int n){
+        luck = n;
+    }
+
+    //Other helpers
 
     public boolean isDead(){
         return hp <= 0;
