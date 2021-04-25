579e2961e147f1a4605885208e6905d54e7c2bd7
yasminwaldeck <yasmin.waldeck@posteo.de>
2021-04-25 22:31:19 +0200

    Add pause between lines

--- src/main/java/Death.java
+++ src/main/java/Death.java
@@ -2,28 +2,35 @@ import java.util.Scanner;
 
 public class Death {
 
+    public void rd(String text){
+        System.out.println(text);
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {
+        }
+    }
 
     public void rip(){
         Scenarios sc = new Scenarios();
         Scanner in = new Scanner(System.in);
-        System.out.println("Rest in peace, adventurer.");
+        rd("Rest in peace, adventurer.");
         System.exit(0);
     }
 
 
     public void ropeDeath(){
-        System.out.println("A hole opens underneath you.");
-        System.out.println("You fall into it and are surrounded by ropes.");
-        System.out.println("Wait...");
-        System.out.println("Ropes don't move...");
-        System.out.println("Those are snakes!!!");
-        System.out.println("They bite you and you die.");
+        rd("A hole opens underneath you.");
+        rd("You fall into it and are surrounded by ropes.");
+        rd("Wait...");
+        rd("Ropes don't move...");
+        rd("Those are snakes!!!");
+        rd("They bite you and you die.");
         rip();
     }
 
     public void orkDeath(){
-        System.out.println("The ork pulls you near and you smell it's horrible breath.");
-        System.out.println("You try to free yourself, but the ork is stronger.");
+        rd("The ork pulls you near and you smell it's horrible breath.");
+        rd("You try to free yourself, but the ork is stronger.");
         rip();
     }
 
--- src/main/java/Scenarios.java
+++ src/main/java/Scenarios.java
@@ -9,16 +9,23 @@ public class Scenarios {
     private Player you = new Player();
     private Death death = new Death();
 
-
+public void rd(String text){
+    System.out.println(text);
+    try {
+        Thread.sleep(1000);
+    } catch(InterruptedException ex) {
+    }
+}
     public void scenario1() {
-        System.out.println("You wake up. Someone is knocking on the door. What do you do?");
+        rd("You wake up. Someone is knocking on the door. What do you do?");
+
         System.out.println("a. Open the door.");
         System.out.println("b. Hide in bed.");
         switch (in.nextLine()) {
             case "a" -> greeting();
             case "b" -> continueKnocking();
             default -> {
-                System.out.println("That is not an option, idiot. Try again.");
+                rd("That is not an option, idiot. Try again.");
                 scenario1();
             }
         }
@@ -26,24 +33,24 @@ public class Scenarios {
 
     public void greeting(){
 
-        System.out.println("\"Hello there!\"");
-        System.out.println("\"What's your name?\"");
+        rd("\"Hello there!\"");
+        rd("\"What's your name?\"");
         String name =  in.nextLine();
         you.setName(name);
-        System.out.println("\"" + you.getName() + ", huh? That is a strange name...\"");
+        rd("\"" + you.getName() + ", huh? That is a strange name...\"");
         death.ropeDeath();
     }
 
     public void continueKnocking(){
 
-        System.out.println("They continue knocking. What do you do?");
+        rd("They continue knocking. What do you do?");
         System.out.println("a. Open the door.");
         System.out.println("b. Hide in bed.");
         switch (in.nextLine()) {
             case "a" -> greeting();
             case "b" -> continueKnocking();
             default -> {
-                System.out.println("That is not an option, idiot. Try again.");
+                rd("That is not an option, idiot. Try again.");
                 scenario1();
             }
         }
